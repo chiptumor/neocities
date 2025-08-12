bbfc14e1f322d4b66bfa22a71cd1937b8dc1201e
Marlie <lizzpegschris@gmail.com>
2025-08-12 13:56:34 -0500

    generic save

--- main.js
+++ main.js
@@ -2,12 +2,16 @@
 	const banner = document.getElementById("banner");
 	const button = banner.querySelector("div.close");
 	const openBanner = () => {
-		banner.className = banner.className.replace("closed", "open");
+		if (window.event.button === 0) {
+			banner.className = banner.className.replace("closed", "open");
+		}
 	};
-	button.addEventListener("click", function (e) {
+	button.addEventListener("mousedown", function (e) {
 		e.stopPropagation();
-		banner.className = banner.className.replace("open", "closed");
+		if (window.event.button === 0) {
+			banner.className = banner.className.replace("open", "closed");
+		}
 	});
 
-	banner.addEventListener("click", openBanner);
+	banner.addEventListener("mousedown", openBanner);
 }
