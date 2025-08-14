c6cc2163f3c2dc03bbaf115b1596bd5f55abe9aa
chiptumor <nightofthebuttlickers@gmail.com>
2025-08-14 18:42:36 -0500

    generic save

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-{ /*** BANNER ***/
+/*** BANNER ***/
+{
 	const banner = document.getElementById("banner");
-	const button = banner.querySelector("div.close");
+	const button = banner.querySelector("div.close > span");
 	const openBanner = () => {
 		if (window.event.button === 0) {
 			banner.className = banner.className.replace("closed", "open");
@@ -16,14 +17,17 @@
 	banner.addEventListener("mousedown", openBanner);
 }
 
-window.onload = async function() { /*** PFP ***/
+/*** PFP ***/
+window.onload = async function() {
 	const aElement = document.getElementById("pfp");
 	const imgElement = aElement.firstElementChild;
 	const response = await fetch("external/pfp/index.json");
-	const array = await response.json();
-	const pfp = array[Math.floor(Math.random() * 14)];
+	const text = await response.text();
+	const array = await JSON5.parse(text);
+	const number = Math.floor(Math.random() * 14);
+	const pfp = array[number];
 
-	imgElement.setAttribute("style", `background-image: url(${pfp.path});`);
+	imgElement.setAttribute("style", `background-image: url(external/pfp/${number}.png);`);
 	aElement.setAttribute("title", pfp.artist);
 	aElement.setAttribute("href", pfp.source);
 };
